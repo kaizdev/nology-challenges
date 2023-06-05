d5c9640b908bfd64bcf007e3c8a55ca6448c9f48
Kai Zhang <kai.zhang@outlook.com.au>
2023-06-05 12:58:51 +1000

    add js operators and variables

---
+++ js/variable/script.js
@@ -0,0 +1,13 @@
+//^  Challenge: Storing Personal Information
+// MVP:
+// Create three variables to store your first name, last name and age.
+// Assign values to these variables.
+// Print a string containing all three variables in the console (should look like "Hi, my name is XXX XXX and I am XX years old)
+
+let firstName = prompt("First name: ");
+let lastName = prompt("Last name: ");
+let age = prompt("Age: ");
+
+console.log(
+    `Hi, my name is ${firstName} ${lastName} and I am ${age} years old`
+);
