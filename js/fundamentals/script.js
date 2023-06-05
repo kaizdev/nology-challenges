8d3b760691cae63f4b9d5a63683deee29e81d138
Kai Zhang <kai.zhang@outlook.com.au>
2023-06-05 10:13:09 +1000

    add js challenge - operators

---
+++ js/fundamentals/script.js
@@ -0,0 +1,13 @@
+console.log(typeof 15); // Prediction: Number
+console.log(typeof 5.5); // Prediction: Number
+console.log(typeof NaN); // Prediction: Number - type conversion went wrong - couldn't convert to number.
+console.log(typeof "hello"); // Prediction: String
+console.log(typeof true); // Prediction: boolean
+console.log(typeof (1 != 2)); // Prediction: boolean
+console.log(typeof ("hamburger" + "s")); // Prediction: string
+console.log(typeof ("hamburgers" - "s")); // Prediction: number (as it is NaN)
+console.log(typeof ("1" + "3")); // Prediction: string ('13')
+console.log(typeof ("1" - "3")); // Prediction: number (as it is NaN)
+console.log(typeof ("johnny" + 5)); // Prediction: string
+console.log(typeof ("johnny" - 5)); // Prediction: number (as it is NaN)
+console.log(typeof (99 * "luftbaloons")); // Prediction: number (as it is NaN)
