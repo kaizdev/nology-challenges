0475f15a2ed2a55baca3ce2dc18d2bd3f3c55468
Kai Zhang <kai.zhang@outlook.com.au>
2023-06-13 14:21:58 +1000

    add iterators challenge

--- js/iterators/iterators.js
+++ js/iterators/iterators.js
@@ -7,3 +7,31 @@ console.log(upperCaseNames);
 const numbers = [1, 10, 20, 30, 40, 50, 60];
 const numFiveLess = numbers.map((num) => num - 5);
 console.log(numFiveLess);
+
+// Extra practice Map
+
+// 1) Make an array of numbers that are doubles of the first array
+function doubleNumbers(arr) {
+    return arr.map((num) => num * 2);
+}
+
+console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+// 2) Take an array of numbers and make them strings
+function stringItUp(arr) {
+    return arr.map((num) => num.toString());
+}
+
+console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+
+// 3) Capitalize each of an array of names
+
+function capitalizeNames(arr) {
+    return arr.map(
+        (name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+    );
+    //need to return the map
+}
+
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+//* Notice that this function needs to return the map!
